7093e3dbd5d967f6b731de5e615da2e8e28814c0
dmalikcs <dmalikcs@gmail.com>
2013-12-24 23:38:38 +0530

    added tastypie/celery application template

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,11 @@
 from fabric.api import local
 
 
-def installapp(app_name=None):
-    template = 'https://github.com/dmalikcs/django-app-template/archive/master.zip'
-    local("python manage.py startapp --template=%s  %s" % (template, app_name, ))
+def installapp(app_name=None, app_type=None):
+    if app_type == 'tastypie':
+        template = 'https://github.com/dmalikcs/django-tastypie-app-template/archive/master.zip'
+    elif app_type == 'celery':
+        template = 'https://github.com/dmalikcs/django-celery-app-template/archive/master.zip'
+    else:
+        template = 'https://github.com/dmalikcs/django-app-template/archive/master.zip'
+    local("python manage.py startapp --template=%s %s" % (template, app_name, ))
