c5c9ce691c5150b74ddde665332d5b5aae070d12
dmalikcs <dmalikcs@gmail.com>
2014-01-01 00:42:07 +0530

    Debug Static files handler add in urls.py

--- project_name/urls.py
+++ project_name/urls.py
@@ -11,3 +11,5 @@ urlpatterns = patterns('',
                        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
                        url(r'^admin/', include(admin.site.urls)),
                        )
+if settings.DEBUG:
+    urlpatterns += patterns('', url(r'%s(?P<path>.*)' % settings.MEDIA_URL[1:], 'django.views.static.serve', {'document_root': setti ngs.MEDIA_ROOT}),)
