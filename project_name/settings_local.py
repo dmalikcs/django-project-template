4b5a5f759d403dc556a914c152c35a0aa8d31bc8
dmalikcs <dmalikcs@gmail.com>
2014-01-06 02:36:13 +0530

    update settings_local.py file

--- project_name/settings_local.py
+++ project_name/settings_local.py
@@ -1,1 +1,14 @@
 from settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db_{{ project_name }}',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'dmalik5',
+        'PASSWORD': 'unix123',
+        'HOST': '192.168.1.102',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',                      # Set to empty string for default.
+        'OPTIONS': {'autocommit': True, }
+    }
+}
