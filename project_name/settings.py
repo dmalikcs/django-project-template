0ce0b3e9bed5e25de9bc699f8e14eac7c3b7457e
dmalikcs <dmalikcs@gmail.com>
2014-01-13 02:26:39 +0530

    webdesign application installed

--- project_name/settings.py
+++ project_name/settings.py
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'django.contrib.webdesign',
 
     #3rd part Application
     'south',
